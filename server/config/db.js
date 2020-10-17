041c426c2f97cdc2794e3d9d60e2c406b92aa921
FSantiago1987 <rawpower_br@hotmail.com>
2020-10-17 12:45:46 -0700

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://fabio:Sw7wOIxUiR9Y6PD0@mongodbserver.7zlzn.azure.mongodb.net/book_store?retryWrites=true&w=majority"
 }
